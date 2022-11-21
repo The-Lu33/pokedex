b1a9c8e92f56e7d266cac5d3261ceeb91ebf0ae3
The Lu <luiseducol13@gmail.com>
2022-11-21 08:33:23 -0500

    change

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -8,8 +8,8 @@ const Characters = () => {
   const [characters, setCharacters] = useState([]);
   useEffect(() => {
     axios
-      //   .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154") //esta es la api
-      .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")//api de 20pokemon
+      //   .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154")
+      .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
       .then((res) => {
         setCharacters(res.data.results);
       });
@@ -21,9 +21,7 @@ const Characters = () => {
       <h2>welcome {userName}</h2>
       <ul>
         {characters.map((character) => (
-          <li key={character.name}>
-            <PokemonCard url={character.url}/>
-            </li>
+          <li key={character.name}><PokemonCard url={character.url}/></li>
         ))}
       </ul>
     </div>
